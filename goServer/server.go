5f28b83b0710d7d4b6748f0744412e14d7ad8e47
Rodrigo Tamura <rodrigo.tamura@ifood.com.br>
2022-03-13 08:12:46 -0700

    feat: v5 of goServer launched

--- goServer/server.go
+++ goServer/server.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8000", nil)
 }
@@ -16,4 +19,14 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	age := os.Getenv("AGE")
 
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file", err)
+	}
+
+	fmt.Fprintf(w, "My family: %s.", string(data))
 }
