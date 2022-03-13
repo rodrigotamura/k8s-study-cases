cfaa6053fe2c306fa91721b717773fd056ef9ad6
Rodrigo Tamura <rodrigo.tamura@ifood.com.br>
2022-03-13 06:22:13 -0700

    chore: added v3 goServer and accessible with service

--- goServer/server.go
+++ goServer/server.go
@@ -4,9 +4,9 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello test!!!"))
+	w.Write([]byte("Hello test at port 8000!"))
 }
